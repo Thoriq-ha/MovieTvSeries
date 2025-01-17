1ca8eb708bfe5eee1effcd47a4f9ed556a58fb08
Thoriq Harizul Ahsan <64089072+Thoriq-ha@users.noreply.github.com>
2025-01-17 17:52:28 +0700

    feat(firebase): add firebase analytic and crashlytic

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,31 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:movietvseries/common/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:movietvseries/firebase_options.dart';
 import 'package:movietvseries/routes/my_router.dart';
 import 'package:movietvseries/injection.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   await di.init();
   await dotenv.load(fileName: ".env");
+
   runApp(MyApp());
 }
 
